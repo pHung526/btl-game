cdffb695cfed6dbcf60402c978c3127bfa006856
pHung526 <phamthehung206@gmail.com>
2025-04-24 09:26:56 +0700

    chia file system

---
+++ src/System.cpp
@@ -0,0 +1,22 @@
+#include "System.h"
+#include "constants.h"  // để dùng SCREEN_WIDTH, SCREEN_HEIGHT
+
+bool init(SDL_Window*& window, SDL_Renderer*& renderer, TTF_Font*& font) {
+    SDL_Init(SDL_INIT_VIDEO);
+    TTF_Init();
+    IMG_Init(IMG_INIT_PNG);
+    window = SDL_CreateWindow("Fruit Slicer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                              SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    font = TTF_OpenFont("E:/fruitss/novem.ttf", 24); // nhớ kiểm tra đường dẫn
+    return window && renderer && font;
+}
+
+void close(SDL_Window* window, SDL_Renderer* renderer, TTF_Font* font) {
+    TTF_CloseFont(font);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    IMG_Quit();
+    TTF_Quit();
+    SDL_Quit();
+}
